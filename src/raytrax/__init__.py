7e6e5521664b99dafc7dfefec44876414922ae3f
David Straub <straub@protonmail.com>
2026-03-11 14:12:18 +0100

    Add grid resolution settings

--- src/raytrax/__init__.py
+++ src/raytrax/__init__.py
@@ -12,7 +12,11 @@ if not getattr(jax.config, "jax_enable_x64", False):
     )
 
 from .api import trace as trace
+from .equilibrium.interpolate import (
+    CylindricalGridResolution as CylindricalGridResolution,
+)
 from .equilibrium.interpolate import MagneticConfiguration as MagneticConfiguration
+from .equilibrium.interpolate import VmecGridResolution as VmecGridResolution
 from .tracer.buffers import Interpolators as Interpolators
 from .types import Beam as Beam
 from .types import RadialProfiles as RadialProfiles
@@ -20,9 +24,11 @@ from .types import TracerSettings as TracerSettings
 
 __all__ = [
     "Beam",
+    "CylindricalGridResolution",
     "Interpolators",
     "MagneticConfiguration",
     "RadialProfiles",
     "TracerSettings",
+    "VmecGridResolution",
     "trace",
 ]
